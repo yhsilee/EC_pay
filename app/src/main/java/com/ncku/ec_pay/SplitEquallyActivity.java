20436d75a77d269a1a5494619d0b28d4bbed0c25
hippo7970 <72782615+hippo7970@users.noreply.github.com>
2020-10-19 15:24:24 +0800

    equallysplit

--- app/src/main/java/com/ncku/ec_pay/SplitEquallyActivity.java
+++ app/src/main/java/com/ncku/ec_pay/SplitEquallyActivity.java
@@ -1,14 +1,24 @@
-package com.ncku.ec_pay;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.Scanner;
 
-import android.os.Bundle;
+/*
+class Group {
+    public int number; //Group Members 有幾位
+}
+*/
 
-public class SplitEqually extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_split_equally);
+public class test {
+    public static void main(String[] args) {
+    	float money;
+    	int people;
+    	float average;
+        System.out.println("請輸入金額:");
+        Scanner howmuch = new Scanner(System.in);
+        System.out.println("請輸入人數:");
+        Scanner howmany = new Scanner(System.in);
+        money = howmuch.nextFloat();
+        people=howmany.nextInt();
+        average=money/people;
+        System.out.print("金額:"+ average);
     }
 }
