9f477adc6754b9a7c2190ff8b5ca83117540c7da
yen861112 <33426923+yen861112@users.noreply.github.com>
2020-10-19 14:10:21 +0800

    新增四個方法

---
+++ app/src/main/java/com/ncku/ec_pay/SplitByPercentagesActivity.java
@@ -0,0 +1,14 @@
+package com.ncku.ec_pay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplitByPercentages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_split_by_percentages);
+    }
+}
---
+++ app/src/main/java/com/ncku/ec_pay/SplitEquallyActivity.java
@@ -0,0 +1,14 @@
+package com.ncku.ec_pay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplitEqually extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_split_equally);
+    }
+}
